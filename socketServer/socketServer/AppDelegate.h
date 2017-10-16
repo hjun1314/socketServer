250bc12ad992d24e8de0aca98bd775bbb8f4a847
Jerry <huangjun0529@126.com>
2017-10-16 12:06:49 +0800

    测试socketServer

---
+++ socketServer/socketServer/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  socketServer
+//
+//  Created by hjun on 2017/10/16.
+//  Copyright © 2017年 hjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ socketServer/socketServer/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SocketServer
+//
+//  Created by hjun on 2017/10/13.
+//  Copyright © 2017年 hjun. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
